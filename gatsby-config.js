49922ee535c22454fac1edcae4302fa5fc110724
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-28 11:00:46 -0700

    added react helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-plugin-mdx",
+    "gatsby-plugin-react-helmet",
     {
       resolve: 'gatsby-plugin-typescript',
       options: {
