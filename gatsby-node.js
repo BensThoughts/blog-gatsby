92c221dd393422bc19bdd34673b59fded5ea3aef
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-11-02 15:46:36 -0700

    added eslint overrides for typescript

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,25 +1,25 @@
 const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
-  const analyzerMode = process.env.ANALYZE ? "server" : "json";
+exports.onCreateWebpackConfig = ({stage, actions}) => {
+  const analyzerMode = process.env.ANALYZE ? 'server' : 'json';
 
   if (stage === 'build-javascript') {
     actions.setWebpackConfig({
       plugins: [
         new BundleAnalyzerPlugin({
           analyzerMode,
-          reportFileName: `./__build/bundlereport.json`,
-        })
-      ]
-    })
+          reportFileName: `./__build/bundleReport.json`,
+        }),
+      ],
+    });
   }
 
   actions.setWebpackConfig({
     resolve: {
       plugins: [
         new TsConfigPathsPlugin(),
-      ]
-    }
-  })
-}
+      ],
+    },
+  });
+};
