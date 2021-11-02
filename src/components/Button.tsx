45d6b696192b9868ff985d7121fe2c88090e0053
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-11-02 16:39:51 -0700

    renamed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-export default function RoundedButton({className, children, ...rest}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+export default function Button({className, children, ...rest}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button className={`font-bold py-2 px-4 rounded inline-flex items-center ${className}`} {...rest}>
       {children}
