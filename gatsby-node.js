3b52b73f9c474ea289dd3a0e0bc42bfa4533646a
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-11-02 19:30:13 -0700

    programmatic generation commented out

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,3 +23,78 @@ exports.onCreateWebpackConfig = ({stage, actions}) => {
     },
   });
 };
+
+// const path = require('path');
+// const {createFilePath} = require('gatsby-source-filesystem');
+
+// exports.onCreateNode = ({node, actions, getNode}) => {
+//   const {createNodeField} = actions;
+
+//   if (node.internal.type === 'Mdx') {
+//     const value = createFilePath({node, getNode});
+
+//     createNodeField({
+//       name: 'slug',
+//       node,
+//       value: `/blog${value}`,
+//     });
+//   }
+// };
+
+// exports.createPages = async ({graphql, actions, reporter}) => {
+//   const {createPage} = actions;
+//   let result;
+
+//   if (process.env.NODE_ENV === 'development') {
+//     result = await graphql(`
+//       query {
+//         allMdx {
+//           edges {
+//             node {
+//               id
+//               fields {
+//                 slug
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `);
+//   } else {
+//     result = await graphql(`
+//       query {
+//         allMdx(
+//           filter: {slug: {regex: "/^(?!drafts/).*/"}}
+//         ) {
+//           edges {
+//             node {
+//               id
+//               fields {
+//                 slug
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `);
+//   }
+
+//   if (result.errors || !result) {
+//     reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query');
+//   }
+
+//   const posts = result.data.allMdx.edges;
+
+//   posts.forEach(({node}, index) => {
+//     createPage({
+//       // This is the slug you created before
+//       // (or `node.frontmatter.slug`)
+//       path: node.fields.slug,
+//       // This component will wrap our MDX content
+//       component: path.resolve(`./src/components/mdx/Layout.tsx`),
+//       // You can use the values in this context in
+//       // our page layout component
+//       context: {id: node.id},
+//     });
+//   });
+// };
